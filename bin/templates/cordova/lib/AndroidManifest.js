08692e69914ee97a50a12d6b2cbcf0afa7c7a1e3
Christopher J. Brody <chris.brody@gmail.com>
2019-02-08 09:54:31 -0500

    .

--- bin/templates/cordova/lib/AndroidManifest.js
+++ bin/templates/cordova/lib/AndroidManifest.js
@@ -102,20 +102,28 @@ AndroidManifest.prototype.getActivity = function () {
     // Copy variable reference to avoid closure issues
     var prefName = sdkPrefName;
 
+    console.log('===========================');
+
     AndroidManifest.prototype['get' + capitalize(prefName)] = function () {
         var usesSdk = this.doc.getroot().find('./uses-sdk');
         return usesSdk && usesSdk.attrib['android:' + prefName];
     };
 
     AndroidManifest.prototype['set' + capitalize(prefName)] = function (prefValue) {
+        console.log('===========================');
+        console.log('sdkPrefName: ' + sdkPrefName)
+        console.log('prefValue: ' + prefValue)
         var usesSdk = this.doc.getroot().find('./uses-sdk');
 
         if (!usesSdk && prefValue) { // if there is no required uses-sdk element, we should create it first
+            console.log('create uses-sdk');
             usesSdk = new et.Element('uses-sdk');
             this.doc.getroot().append(usesSdk);
         }
 
         if (prefValue) {
+            console.log('set prefName: ' + prefName);
+            console.log('set prefValue: ' + prefValue);
             usesSdk.attrib['android:' + prefName] = prefValue;
         }
 
